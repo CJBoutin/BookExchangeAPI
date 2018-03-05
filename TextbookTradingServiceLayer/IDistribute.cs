3293f512c30fcebe19d765ff38253c83450532eb
Connor Boutin <cjboutin@bellsouth.net>
2018-03-05 13:20:11 -0500

    Add project files.

---
+++ TextbookTradingServiceLayer/IDistribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TextbookTradingServiceLayer
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDistribute
+    {
+
+        // Asks the WCF service if it's alive
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Bare,
+            Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "IsAlive")]
+        string IsAlive();
+    }
+
+
+}
